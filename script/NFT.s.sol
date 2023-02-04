3170de2c0e06b41500dfd636c543af811b513889
ihossain-6 <ismumhossain@gmail.com>
2023-02-04 14:53:47 +0600

    Initial commit

---
+++ script/NFT.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/NFT.sol";
+
+contract NFTScript is Script {
+    function run() public {
+        vm.startBroadcast();
+        new NFT("NFT Sell Token", "NST");
+        vm.stopBroadcast();
+    }
+}
+
+
+
---
+++ script/NFTSell.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "../lib/forge-std/src/Script.sol";
+import "../src/NFTSell.sol";
+import "../src/NFT.sol";
+
+contract NFTScript is Script {
+    NFT public nft;
+
+    function run() public {
+        vm.startBroadcast();
+        new NFTSell(nft);
+        vm.stopBroadcast();
+    }
+}
